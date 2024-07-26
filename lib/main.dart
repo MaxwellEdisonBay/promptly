7e319596fcae49e01c575ee2afccf6941bcc05e5
Ivan Teslenko <iteslenko.dev@gmail.com>
2024-07-25 20:54:21 -0400

    Added entry point and custom fonts support

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:promptly/utils/theme/theme.dart';
+
+import 'app.dart';
 
 void main() {
+  // TODO: Add widget bindings
   runApp(const App());
 }
-
-class App extends StatelessWidget {
-  const App({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      themeMode: ThemeMode.system,
-      theme: TAppTheme.lightTheme,
-      darkTheme: TAppTheme.darkTheme,
-    );
-  }
-}
-
