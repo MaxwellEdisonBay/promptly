d37fae9c3b6d1da59c8c89da7ad8fa1792126fbe
Ivan Teslenko <iteslenko.dev@gmail.com>
2024-07-28 15:43:46 -0400

    Added icons & helper functions

---
+++ lib/utils/constants/image_strings.dart
@@ -0,0 +1,9 @@
+class TImages {
+
+  // App Logos
+  static const String darkAppLogo = "assets/logos/promptly-splash-logo-dark.png";
+  static const String lightAppLogo = "assets/logos/promptly-splash-logo-light.png";
+
+  static const String google = 'assets/logos/google-icon.webp';
+  static const String facebook = 'assets/logos/facebook-icon.png';
+}
---
+++ lib/utils/constants/text_strings.dart
@@ -0,0 +1,11 @@
+class TTexts {
+
+  // -- GLOBAL TEXTS
+
+  // -- Onboarding texts
+  static const String onboardingTitle1 = "Pick your Topics";
+  static const String onboardingTitle2 = "Interact with Templates";
+  static const String onboardingTitle3 = "Create & Share Templates!";
+  
+
+}
